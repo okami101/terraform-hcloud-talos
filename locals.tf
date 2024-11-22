7eb542742b0f092b51a0637556ee21fb22fefc8b
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-11-23 00:56:40 +0100

    post mach

--- locals.tf
+++ locals.tf
@@ -80,7 +80,7 @@ locals {
         machine = local.machine_config
         cluster = local.config_patches["cluster_controlplane_config"]
       })]
-      machine_config = s.machine_config != null ? s.machine_config : {}
+      machine_config = s.machine_config
     }
   ]
   agents = flatten([
@@ -102,7 +102,7 @@ locals {
             cluster = local.config_patches["cluster_worker_config"]
           }
         )]
-        machine_config = s.machine_config != null ? s.machine_config : {}
+        machine_config = s.machine_config
       }
     ]
   ])
