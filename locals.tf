eebe1b7448bdb8d8b74514c57f24b2969e0382ba
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2024-11-23 02:03:59 +0100

    retry taints

--- locals.tf
+++ locals.tf
@@ -76,8 +76,8 @@ locals {
         hcloud_network_subnet.control_plane.ip_range, i + 101
       )
       volume_size = 0
-      labels      = s.labels
-      taints      = s.taints
+      labels      = {}
+      taints      = {}
       config_patches = [yamlencode({
         machine = local.machine_config
         cluster = local.config_patches["cluster_controlplane_config"]
