637ad59a639cdde994dce59f1a13f2c39cc3f7ab
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2025-03-09 18:28:18 +0100

    change fw behavior

--- main.tf
+++ main.tf
@@ -2,30 +2,6 @@ locals {
   network_ipv4_subnets = [
     for index in range(256) : cidrsubnet(var.network_ipv4_cidr, 8, index)
   ]
-  firewall_kube_api_rule = [
-    {
-      description = "Allow Incoming Requests to Kube API Server"
-      direction   = "in"
-      protocol    = "tcp"
-      port        = "6443"
-      source_ips = var.firewall_kube_api_source == null ? [
-        "127.0.0.1",
-        "::1"
-      ] : var.firewall_kube_api_source
-    },
-  ]
-  firewall_talos_api_rule = [
-    {
-      description = "Allow Incoming Talos API Traffic"
-      direction   = "in"
-      protocol    = "tcp"
-      port        = "50000"
-      source_ips = var.firewall_talos_api_source == null ? [
-        "127.0.0.1",
-        "::1"
-      ] : var.firewall_talos_api_source
-    }
-  ]
 }
 
 resource "hcloud_network" "talos" {
@@ -51,30 +27,30 @@ resource "hcloud_network_subnet" "agent" {
 resource "hcloud_firewall" "talos_api" {
   name = "${var.cluster_name}-talos-api"
 
-  dynamic "rule" {
-    for_each = local.firewall_talos_api_rule
-    content {
-      description = rule.value.description
-      direction   = rule.value.direction
-      protocol    = rule.value.protocol
-      port        = rule.value.port
-      source_ips  = rule.value.source_ips
-    }
+  rule {
+    description = "Allow Incoming Talos API Traffic"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "50000"
+    source_ips = var.firewall_talos_api_source == null ? [
+      "127.0.0.1",
+      "::1"
+    ] : var.firewall_talos_api_source
   }
 }
 
 resource "hcloud_firewall" "kube_api" {
   name = "${var.cluster_name}-kube-api"
 
-  dynamic "rule" {
-    for_each = local.firewall_kube_api_rule
-    content {
-      description = rule.value.description
-      direction   = rule.value.direction
-      protocol    = rule.value.protocol
-      port        = rule.value.port
-      source_ips  = rule.value.source_ips
-    }
+  rule {
+    description = "Allow Incoming Requests to Kube API Server"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "6443"
+    source_ips = var.firewall_kube_api_source == null ? [
+      "127.0.0.1",
+      "::1"
+    ] : var.firewall_kube_api_source
   }
 }
 
